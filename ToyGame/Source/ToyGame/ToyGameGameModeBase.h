334b255f1e820e4219ab11fb7f15721d016395ac
SangEun <kooc547@gmail.com>
2021-12-26 23:33:11 +0900

    init commit

---
+++ ToyGame/Source/ToyGame/ToyGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ToyGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOYGAME_API AToyGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
